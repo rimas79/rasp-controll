68a2bc399a3a9e667bbb58a9378c38f0f239355d
rimas79 <derokhin@gmail.com>
2014-03-09 13:27:01 +0400

    work with server

--- raspcontroll/src/main/java/com/rc/raspcontroll/MainActivity.java
+++ raspcontroll/src/main/java/com/rc/raspcontroll/MainActivity.java
@@ -3,44 +3,53 @@ package com.rc.raspcontroll;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.os.Handler;
-import android.os.Message;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.ToggleButton;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends ActionBarActivity {
 
     static final String APP_PREFERENCES = "mysettings";
     static final String APP_PREFERENCES_URL = "rasp_url";
     static final String APP_PREFERENCES_TOK = "rasp_tok";
-    private static final String MSG_TAG = "MSG_HANDLER";
+
+/*
+    команды
+*/
+
+    static final String get_token = "get_token";
+    static final String get_sw = "get_sw";
+    static final String set_led = "set_led";
+
     SharedPreferences mSettings;
 
-    public Handler _handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            Log.d(MSG_TAG, String.format("Handler.handleMessage(): msg=%s", msg));
-            // This is where main activity thread receives messages
-            // Put here your handling of incoming messages posted by other threads
-            super.handleMessage(msg);
-        }
-
-    };
+    RaspHTTPClient rasp;
 
     static private String raps_token = "";
+
+    private String uri;
+
+    private EditText edURL;
 
     static public void setRaps_token(String token) {
         MainActivity.raps_token = token;
@@ -50,13 +59,13 @@ public class MainActivity extends ActionBarActivity {
         Switch sw = (Switch) findViewById(R.id.swConnect);
         sw.setChecked(status);
         if (!status){
-            ImageView iv = (ImageView) findViewById(R.id.ivSwitchStatus);
+            ImageView iv = (ImageView) findViewById(R.id.ivSwStatus);
             iv.setBackgroundColor(0xaaa);
         }
     }
 
     void setSwitch(Boolean status){
-        ImageView iv = (ImageView) findViewById(R.id.ivSwitchStatus);
+        ImageView iv = (ImageView) findViewById(R.id.ivSwStatus);
         if (status){
             iv.setBackgroundColor(0xff99cc00);
         }
@@ -82,6 +91,7 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        rasp = new RaspHTTPClient();
     }
 
 
@@ -94,23 +104,27 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onResume(){
         super.onResume();
+
+        edURL = (EditText) findViewById(R.id.raspURL);
+
+        edURL.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                uri = editable.toString();
+            }
+        });
+
         if (mSettings.contains(APP_PREFERENCES_URL)) {
-            EditText ed = (EditText) findViewById(R.id.raspURL);
-            ed.setText(mSettings.getString(APP_PREFERENCES_URL, ""));
+            uri = mSettings.getString(APP_PREFERENCES_URL, "");
+            edURL.setText(uri);
         }
 
         if (mSettings.contains(APP_PREFERENCES_TOK)) {
@@ -123,16 +137,14 @@ public class MainActivity extends ActionBarActivity {
     public void onPause() {
         // TODO Auto-generated method stub
         super.onPause();
-
-        EditText ed = (EditText) findViewById(R.id.raspURL);
-
         SharedPreferences.Editor editor = mSettings.edit();
-        editor.putString(APP_PREFERENCES_URL, ed.getText().toString());
+        editor.putString(APP_PREFERENCES_URL, edURL.getText().toString());
         editor.putString(APP_PREFERENCES_TOK, raps_token);
         editor.apply();
     }
 
     public void tbConn_Click(View view) {
+        rasp.execute(uri+'/'+get_token);
         return;
     }
 
@@ -154,5 +166,87 @@ public class MainActivity extends ActionBarActivity {
         }
 
     }
+
+    public class RaspHTTPClient extends AsyncTask<String, Void, String> {
+
+        // Reads an InputStream and converts it to a String.
+        public String readIt(InputStream stream, int len) throws IOException {
+            Reader reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
+
+        private String downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            // Only display the first 500 characters of the retrieved
+            // web page content.
+            int len = 500;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... urls){
+            try {
+                return downloadUrl(urls[0]);
+            }
+            catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+
+            if (result.equals("OK")) {
+                setConnected(true);
+            }
+            else if (result.startsWith("TOKEN")) {
+                String[] res_tok = result.split("=", 2);
+                setRaps_token(res_tok[1]);
+            }
+            else if (result.equals("SW_ON")){
+                setSwitch(true);
+            }
+            else if (result.equals("SW_OFF")){
+                setSwitch(false);
+            }
+            else if (result.equals("LED_ON")){
+                setLED(true);
+            }
+            else if (result.equals("LED_OFF")){
+                setLED(false);
+            }
+            else {
+                setConnected(false);
+            }
+        }
+
+    }
+
 
 }
