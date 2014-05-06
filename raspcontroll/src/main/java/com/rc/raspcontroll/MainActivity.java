3fcd698f158a587a91c760ea018c670682b6b921
rimas79 <derokhin@gmail.com>
2014-05-06 20:38:34 +0400

    hx led controller support

--- raspcontroll/src/main/java/com/rc/raspcontroll/MainActivity.java
+++ raspcontroll/src/main/java/com/rc/raspcontroll/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -15,8 +16,11 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.ToggleButton;
 
@@ -27,11 +31,12 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements SeekBar.OnSeekBarChangeListener, Spinner.OnItemSelectedListener {
 
     static final String APP_PREFERENCES = "mysettings";
     static final String APP_PREFERENCES_URL = "rasp_url";
-    static final String APP_PREFERENCES_TOK = "rasp_tok";
+
+    private static String androidID;
 
 /*
     команды
@@ -40,10 +45,28 @@ public class MainActivity extends ActionBarActivity {
     static final String get_token = "get_token";
     static final String get_sw = "get_sw";
     static final String set_led = "set_led";
+// led bar
+    static final String LED_BAR = "led_bar";
+    static final String LED_BAR_DYN = "dynamic";
+    static final String LED_BAR_RED = "red";
+    static final String LED_BAR_GREEN = "green";
+    static final String LED_BAR_BLUE = "blue";
+    static final String LED_BAR_BRIGHT = "bright";
+    static final String LED_BAR_SPEED = "speed";
+    static final String LED_BAR_DYN_MODE = "dyn_mode";
+    static final String LED_BAR_DYN_EFF = "dyn_effect";
+//
+
+    SeekBar seekBarRed;
+    SeekBar seekBarGreen;
+    SeekBar seekBarBlue;
+    SeekBar seekBarBright;
+
+    Spinner spMode;
+    Spinner spSpeed;
+    Spinner spEffect;
 
     SharedPreferences mSettings;
-
-    RaspHTTPClient rasp;
 
     static private String raps_token = "";
 
@@ -55,28 +78,74 @@ public class MainActivity extends ActionBarActivity {
         MainActivity.raps_token = token;
     }
 
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+                                  boolean fromUser) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+        RaspHTTPClient rasp;
+        rasp = new RaspHTTPClient();
+        String command = uri;
+        command += "/"+"LED_BAR?";
+        command += LED_BAR_DYN+"=false&";
+        command += LED_BAR_RED+"="+seekBarRed.getProgress()+"&";
+        command += LED_BAR_GREEN+"="+seekBarGreen.getProgress()+"&";
+        command += LED_BAR_BLUE+"="+seekBarBlue.getProgress()+"&";
+        command += LED_BAR_BRIGHT+"="+seekBarBright.getProgress();
+        rasp.execute(command);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int position, long id) {
+        // показываем позиция нажатого элемента
+        RaspHTTPClient rasp;
+        rasp = new RaspHTTPClient();
+        String command = uri+"/"+"LED_BAR?"+LED_BAR_DYN+"=true&"+LED_BAR_SPEED+"="+(spSpeed.getSelectedItemPosition()+1)+"&"+LED_BAR_DYN_MODE+"="+(spMode.getSelectedItemPosition()+1)+"&"+LED_BAR_DYN_EFF+"="+(spEffect.getSelectedItemPosition()+1);
+     //   command = command + LED_BAR_RED+"="+seekBarRed.getProgress()+"&"+LED_BAR_GREEN+"="+seekBarGreen.getProgress()+"&"+LED_BAR_BLUE+"="+seekBarBlue.getProgress();
+        rasp.execute(command);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+    }
+
     void setConnected(Boolean status){
         Switch sw = (Switch) findViewById(R.id.swConnect);
         sw.setChecked(status);
         if (!status){
-            ImageView iv = (ImageView) findViewById(R.id.ivSwStatus);
-            iv.setBackgroundColor(0xaaa);
+//            ImageView iv = (ImageView) findViewById(R.id.ivSwStatus);
+//            iv.setBackgroundColor(0xaaa);
         }
+
     }
 
     void setSwitch(Boolean status){
-        ImageView iv = (ImageView) findViewById(R.id.ivSwStatus);
+/*        ImageView iv = (ImageView) findViewById(R.id.ivSwStatus);
         if (status){
             iv.setBackgroundColor(0xff99cc00);
         }
         else {
             iv.setBackgroundColor(0xffff4444);
         }
+*/
     }
 
     void setLED(Boolean status){
-        ToggleButton tb = (ToggleButton) findViewById(R.id.tbLED);
+/*        ToggleButton tb = (ToggleButton) findViewById(R.id.tbLED);
         tb.setChecked(status);
+*/
     }
 
     @Override
@@ -91,9 +160,11 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-        rasp = new RaspHTTPClient();
-    }
 
+        androidID = Settings.System.getString(getContentResolver(), Settings.System.ANDROID_ID);
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -106,6 +177,22 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public void onResume(){
         super.onResume();
+
+        seekBarRed = (SeekBar)findViewById(R.id.seekBarRed);
+        seekBarRed.setOnSeekBarChangeListener(this);
+        seekBarGreen = (SeekBar)findViewById(R.id.seekBarGreen);
+        seekBarGreen.setOnSeekBarChangeListener(this);
+        seekBarBlue = (SeekBar)findViewById(R.id.seekBarBlue);
+        seekBarBlue.setOnSeekBarChangeListener(this);
+        seekBarBright = (SeekBar)findViewById(R.id.seekBarBright);
+        seekBarBright.setOnSeekBarChangeListener(this);
+
+        spMode = (Spinner)findViewById(R.id.spinnerModes);
+        spMode.setOnItemSelectedListener(this);
+        spSpeed = (Spinner)findViewById(R.id.spinnerSpeeds);
+        spSpeed.setOnItemSelectedListener(this);
+        spEffect = (Spinner)findViewById(R.id.spinnerEffects);
+        spEffect.setOnItemSelectedListener(this);
 
         edURL = (EditText) findViewById(R.id.raspURL);
 
@@ -127,9 +214,6 @@ public class MainActivity extends ActionBarActivity {
             edURL.setText(uri);
         }
 
-        if (mSettings.contains(APP_PREFERENCES_TOK)) {
-            raps_token = mSettings.getString(APP_PREFERENCES_TOK, "");
-        }
     }
 
     @TargetApi(Build.VERSION_CODES.GINGERBREAD)
@@ -139,12 +223,13 @@ public class MainActivity extends ActionBarActivity {
         super.onPause();
         SharedPreferences.Editor editor = mSettings.edit();
         editor.putString(APP_PREFERENCES_URL, edURL.getText().toString());
-        editor.putString(APP_PREFERENCES_TOK, raps_token);
         editor.apply();
     }
 
     public void tbConn_Click(View view) {
-        rasp.execute(uri+'/'+get_token);
+        RaspHTTPClient rasp;
+        rasp = new RaspHTTPClient();
+        rasp.execute(uri+"/"+get_token+"?andr_id="+androidID);
         return;
     }
 
@@ -211,10 +296,11 @@ public class MainActivity extends ActionBarActivity {
         @Override
         protected String doInBackground(String... urls){
             try {
-                return downloadUrl(urls[0]);
+                String res = downloadUrl(urls[0]);
+                return res;
             }
             catch (IOException e) {
-                return "Unable to retrieve web page. URL may be invalid.";
+                return "Unable to retrieve web page:"+e.toString();
             }
         }
 
@@ -228,6 +314,7 @@ public class MainActivity extends ActionBarActivity {
             else if (result.startsWith("TOKEN")) {
                 String[] res_tok = result.split("=", 2);
                 setRaps_token(res_tok[1]);
+                setConnected(true);
             }
             else if (result.equals("SW_ON")){
                 setSwitch(true);
@@ -240,6 +327,9 @@ public class MainActivity extends ActionBarActivity {
             }
             else if (result.equals("LED_OFF")){
                 setLED(false);
+            }
+            else if (result.equals("LED_BAR_OK")){
+//                setLED(false);
             }
             else {
                 setConnected(false);
